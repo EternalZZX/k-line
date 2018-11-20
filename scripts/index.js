03ce9427b108f015d1639afd8c4102598d129148
EternalZZX <1301340187@qq.com>
2018-11-20 16:47:57 +0800

    print

--- scripts/index.js
+++ scripts/index.js
@@ -651,15 +651,16 @@ function callback (res) {
 }
 
 function exportPdf () {
-  var doc = new jsPDF();          
-  var elementHandler = {
-    '#ignorePDF': function (element, renderer) {
-      return true;
-    }
-  };
-  var source = window.document.getElementsByTagName("body")[0];
-  doc.fromHTML(source, 15, 15, {
-    'width': 180, 'elementHandlers': elementHandler
-  });
-  doc.output("dataurlnewwindow");
+  window.print();
+  // var doc = new jsPDF();          
+  // var elementHandler = {
+  //   '#ignorePDF': function (element, renderer) {
+  //     return true;
+  //   }
+  // };
+  // var source = window.document.getElementsByTagName("body")[0];
+  // doc.fromHTML(source, 15, 15, {
+  //   'width': 180, 'elementHandlers': elementHandler
+  // });
+  // doc.output("dataurlnewwindow");
 }
